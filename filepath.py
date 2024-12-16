53471a91002bf335b91a6cd222165389bc5fd394
Zach Vanderbosch <zvanderbosch@nps.gov>
2024-12-16 13:26:37 -0700

    Temporary filepath script

---
+++ filepath.py
@@ -0,0 +1,10 @@
+# Stand-in file to set up file paths for the CCD pipeline
+#
+# Z Vanderbosch
+
+lincurve = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Images/Linearity Curves/"
+flats = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Images/Master/"
+rawdata = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/FieldData/"
+calibdata = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/Calibdata/"
+tiff = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/Calibdata/"
+apikey = 'kdvqtjbqkbkbuyzb' 		#Astrometry API key
