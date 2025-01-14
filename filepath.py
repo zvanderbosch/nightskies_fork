88e0a5f3a1492832c5e96b024e59d13f84fa3362
Zach Vanderbosch <zvanderbosch@nps.gov>
2025-01-14 15:25:33 -0700

    Add raster and griddata paths

--- filepath.py
+++ filepath.py
@@ -2,10 +2,13 @@
 #
 # Z Vanderbosch
 
-lincurve = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Images/Linearity Curves/"
-flats = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Images/Master/"
-rawdata = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/FieldData/"
-calibdata = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/Calibdata/"
-tiff = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/Calibdata/"
-standards = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig/Data/Standards/"
-apikey = 'kdvqtjbqkbkbuyzb' 		#Astrometry API key
+base = "C:/Users/zvanderbosch/OneDrive - DOI/Documents/NSNSD_CCD/pipeline_orig"
+lincurve = f"{base}/Images/Linearity Curves/"
+flats = f"{base}/Images/Master/"
+rawdata = f"{base}/Data/FieldData/"
+calibdata = f"{base}/Data/Calibdata/"
+griddata = f"{base}/Data/griddata/"
+tiff = f"{base}/Data/Calibdata/"
+standards = f"{base}/Data/Standards/"
+rasters = f"{base}/Data/rasters/"
+apikey = 'kdvqtjbqkbkbuyzb'  #Astrometry API key
