80bf639e6b960c1201e41f8e00c225e9d0edd121
Zach Vanderbosch <zvanderbosch@nps.gov>
2025-01-22 10:38:46 -0700

    Use PIL for TIFF image reading

--- medianmosaic.py
+++ medianmosaic.py
@@ -28,11 +28,13 @@
 #-----------------------------------------------------------------------------#
 from astropy.io import fits
 from tqdm import trange
+from PIL import Image
 from skimage.transform import downscale_local_mean
 
 import arcpy
 import numpy as n
 import os
+import stat
 import shutil
 
 # Local Source
@@ -241,7 +243,8 @@ def mosaic(dnight, sets, filter):
         #mosaic raster list must start with an image with max pixel value > 256
         v=1; mstart=1
         while v < (len(Obs_AZ)+1):
-            im = imread(filepath.rasters+'scratch_median/ib%03d.tif' %v)
+            tiff = Image.open(filepath.rasters+'scratch_median/ib%03d.tif' %v)
+            im = n.array(tiff)
             if n.max(im) > 255:
                 mstart = v
                 break
