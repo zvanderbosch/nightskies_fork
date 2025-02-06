526b8eb680c3388cb0836d49eb6e2c974386a158
Zach Vanderbosch <zvanderbosch@nps.gov>
2025-02-06 11:08:22 -0700

    Script with printing colors

---
+++ printcolors.py
@@ -0,0 +1,30 @@
+#-----------------------------------------------------------------------------#
+#print_colors.py
+#
+#NPS Night Skies Program
+#
+#Last updated: 2025/02/06
+#
+#This script defines some colors used when printing status
+#report to the command line during pipeline execution
+#
+#Input: 
+#   None
+#
+#Output:
+#   None
+#
+#History:
+#   Zach Vanderbosch -- Created file 2025/02/06
+#
+#-----------------------------------------------------------------------------#
+
+MAGENTA = '\033[95m'
+BLUE = '\033[94m'
+CYAN = '\033[96m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+END = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
