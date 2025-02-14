cfaf10fb2f29fc35041abc31ed0f322112f046d2
Zach Vanderbosch <zvanderbosch@nps.gov>
2025-02-14 15:25:16 -0700

    More robust file checking

--- zodiacal.py
+++ zodiacal.py
@@ -141,6 +141,23 @@ def clear_scratch(scratch_dir):
             os.chmod(os.path.join(root, name), stat.S_IWRITE)
             os.rmdir(os.path.join(root, name))
 
+def check_file(filename):
+    ''' 
+    Check that a file exists and is no longer
+    being written to by another program.
+    '''
+    
+    if os.path.isfile(filename):
+        size_check_1 = os.path.getsize(filename)
+        time.sleep(1)
+        size_check_2 = os.path.getsize(filename)
+        if size_check_2 > size_check_1:
+            return False
+        else: 
+            return True
+    else: 
+        return False
+
 
 def mosaic(dnight, sets):
     '''
@@ -221,11 +238,8 @@ def mosaic(dnight, sets):
             # Check that clipFile exists
             clipFile = f'{domainsetp}ib{v:03d}/ib{v:03d}_border'
             while True:
-                if(os.path.isfile(f"{clipFile}.shp")) & (os.path.getsize(f"{clipFile}.shp") > 100000):
-                    break
-                else: 
-                    time.sleep(1); 
-                    continue
+                if check_file(f"{clipFile}.shp"): break
+                else: time.sleep(1); continue
 
             arcpy.management.Clip(
                 f"zod{v:02d}.tif", 
