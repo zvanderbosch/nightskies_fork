876ddd562a468d8a77a46ddf7e0b9ceddc6d439d
Zach Vanderbosch <zvanderbosch@nps.gov>
2025-05-22 11:27:59 -0600

    Add datanight counters

--- process_metrics.py
+++ process_metrics.py
@@ -78,6 +78,7 @@ def update_progressbar(x,y,t=0):
 
 def process_skyglow(*args):
     '''Calculate luminance/illuminance metrics for artificial skyglow'''
+    i = args[-1]
     update_progressbar(0,i)
     t1 = time.time()
     import skyglow as SG
@@ -90,6 +91,7 @@ def process_skyglow(*args):
 
 def process_illumall(*args):
     '''Calculate luminance/illuminance metrics for all light sources'''
+    i = args[-1]
     update_progressbar(1,i)
     t1 = time.time()
     import illumall as IA
@@ -102,6 +104,7 @@ def process_illumall(*args):
 
 def process_starsvis(*args):
     '''Calculate visible stars'''
+    i = args[-1]
     update_progressbar(2,i)
     t1 = time.time()
     import starsvis as SV
@@ -114,10 +117,11 @@ def process_starsvis(*args):
 
 def process_alrmodel(*args):
     '''Calculate All-Sky Light Pollution Ratio (ALR) model'''
+    i = args[-1]
     update_progressbar(3,i)
     t1 = time.time()
     import alrmodel as AM
-    AM.calculate_alr_model(*args)
+    AM.calculate_alr_model(*args[:-1])
     t2 = time.time()
     update_progressbar(3,i,t2-t1)
     print(f'{PREFIX}Processing Time (alrmodel): {t2-t1:.2f} seconds')
@@ -125,10 +129,11 @@ def process_alrmodel(*args):
 
 def process_albedomodel(*args):
     '''Calculate albedo model'''
+    i = args[-1]
     update_progressbar(4,i)
     t1 = time.time()
     import albedomodel as BM
-    BM.calculate_albedo_model(*args)
+    BM.calculate_albedo_model(*args[:-1])
     t2 = time.time()
     update_progressbar(4,i,t2-t1)
     print(f'{PREFIX}Processing Time (albedomodel): {t2-t1:.2f} seconds')
@@ -136,21 +141,35 @@ def process_albedomodel(*args):
 
 def process_places(*args):
     '''Calculate distance & Walker's Law for places'''
+    i = args[-1]
     update_progressbar(5,i)
     t1 = time.time()
     import places as PL
-    PL.calculate_places(*args)
+    PL.calculate_places(*args[:-1])
     t2 = time.time()
     update_progressbar(5,i,t2-t1)
     print(f'{PREFIX}Processing Time (places): {t2-t1:.2f} seconds')
 
 
+def process_skyquality(*args):
+    '''Calculate SQI and SQM sky quality metrics'''
+    i = args[-1]
+    update_progressbar(6,i)
+    t1 = time.time()
+    import skyquality as SQ
+    SQ.calculate_sky_quality(*args[:-1])
+    t2 = time.time()
+    update_progressbar(6,i,t2-t1)
+    print(f'{PREFIX}Processing Time (places): {t2-t1:.2f} seconds')
+
+
 def process_drawmaps(*args):
     '''Generate panoramic graphics'''
+    i = args[-1]
     update_progressbar(7,i)
     t1 = time.time()
     import drawmaps as DM
-    DM.generate_graphics(*args)
+    DM.generate_graphics(*args[:-1])
     t2 = time.time()
     update_progressbar(7,i,t2-t1)
     print(f'{PREFIX}Processing Time (drawmaps): {t2-t1:.2f} seconds')
@@ -162,6 +181,7 @@ def process_drawmaps(*args):
 ##########################  Main Program  #####################################
 
 if __name__ == '__main__':
+
     t1 = time.time()
     #-----------------------------------------------------------------------------#    
     print(
@@ -172,11 +192,11 @@ if __name__ == '__main__':
         
     #------------ Read in the processing list and initialize ---------------------#
 
-    #Read in the processing dataset list and the calibration file names 
+    # Read in the processing dataset list and the calibration file names 
     filelist = n.loadtxt(filepath.processlist+'filelist.txt', dtype=str, ndmin=2)
     Dataset, V_band, B_band, _, _, _, processor, centralAz, location = filelist.T
     
-    #Determine the number of data sets collected in each night 
+    # Determine the number of data sets collected in each night 
     img_sets = set(['1st','2nd','3rd','4th','5th','6th','7th','8th'])
     dnight_sets = {}
     nsets = []
@@ -192,18 +212,18 @@ if __name__ == '__main__':
         if not os.path.exists(filepath.calibdata+dnight):
             os.makedirs(filepath.calibdata+dnight)
 
-    #Plot the progress bar template
+    # Plot the progress bar template
     barfig, barax = progressbars.bar_metrics(Dataset, nsets)
     print('You have 5 seconds to adjust the position of the progress bar window')
     plt.pause(5) #users have 5 seconds to adjust the figure position
 
-    #Progress bar array (to be filled with processing time)
+    # Progress bar array (to be filled with processing time)
     Z = n.empty((5+len(filelist),14))*n.nan
     
     
     #------------ Main data processing code --------------------------------------#
 
-    #Looping through multiple data nights
+    # Looping through multiple data nights
     for i in range(len(filelist)):
         
         # Generate inputs for each processing step
@@ -214,10 +234,10 @@ if __name__ == '__main__':
             Filter.append('B')
         
         sets = dnight_sets[Dataset[i]]
-        # K0 = (Dataset[i],sets,Filterset,Curve[i])
-        K0 = (Dataset[i],)  
-        K1 = (Dataset[i],sets,Filter)  
-        K2 = (Dataset[i],sets,processor[0],int(centralAz[0]),location[0])  
+        K0 = (Dataset[i],i)
+        K1 = (Dataset[i],sets,i)
+        K2 = (Dataset[i],sets,Filter,i)
+        K3 = (Dataset[i],sets,processor[0],int(centralAz[0]),location[0],i)
 
         # Status update
         print(
@@ -226,18 +246,18 @@ if __name__ == '__main__':
         )
 
         # Create multiprocessing objects for each step
-        p1 = Process(target=process_skyglow,args=K1)
-        p2 = Process(target=process_illumall,args=K1)
-        p3 = Process(target=process_starsvis,args=K1)
+        p1 = Process(target=process_skyglow,args=K2)
+        p2 = Process(target=process_illumall,args=K2)
+        p3 = Process(target=process_starsvis,args=K2)
         p4 = Process(target=process_alrmodel,args=K0)
         p5 = Process(target=process_albedomodel,args=K0)
         p6 = Process(target=process_places,args=K0)
-        # p7 = Process(target=process_sqi,args=K0)
-        p8 = Process(target=process_drawmaps,args=K2)
+        p7 = Process(target=process_skyquality,args=K1)
+        p8 = Process(target=process_drawmaps,args=K3)
 
         # Execute each processing step
-        p1.start()  # Anthropogenic skyglow luminance & illuminance
-        p1.join()
+        # p1.start()  # Anthropogenic skyglow luminance & illuminance
+        # p1.join()
         # p2.start()  # All sources skyglow luminance & illuminance
         # p2.join()
         # p3.start()  # Number/fraction of visible stars
@@ -248,12 +268,12 @@ if __name__ == '__main__':
         # p5.join()
         # p6.start()  # Places
         # p6.join()
-        # p7.start()  # SQI
-        # p7.join()
+        p7.start()  # Sky quality metrics
+        p7.join()
         # p8.start()  # Draw maps
         # p8.join()
 
-        #save the timing records for running the script
+        # Save the timing records for running the script
         n.savetxt(filepath.calibdata+Dataset[i]+'/processtime_metrics.txt', Z, fmt='%4.1f')
         barfig.savefig(filepath.calibdata+Dataset[i]+'/processtime_metrics.png')
 
