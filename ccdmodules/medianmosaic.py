a5ab29e9014d2fa663fe9e1d96fea0d8a5aad8ea
Zach Vanderbosch <zvanderbosch@nps.gov>
2025-07-24 12:22:43 -0600

    Add parameter definitions

--- ccdmodules/medianmosaic.py
+++ ccdmodules/medianmosaic.py
@@ -108,14 +108,27 @@ target_pnt = (
     "'419597 419597';'-419597 419597';'-419597 -419597';'419597 -419597'"
 )
           
-#-----------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#-------------------            Define Functions            -------------------#
+#------------------------------------------------------------------------------#
+
 def clip_envelope(AZ, ALT, i):
     '''
-    Calculate the four coordinates defining the minimum bounding rectangle to be clipped
-    by the arcpy.Clip_management() function. 
+    Function to generate rectangular clipping boundaries for an image.
 
-    Returns coordinates are defined in this order: 
-    X-Minimum, Y-Minimum, X-Maximum, Y-Maximum.
+    Parameters:
+    -----------
+    AZ: array
+        Array of azimuth coordinates for each image center
+    ALT: array
+        Array of altitude coordinates for each image center
+    i: int
+        Image number
+
+    Returns:
+    --------
+    boundary: str
+        Coordinate boundaries for a given image
     '''
     if i < 15:
         bond = [AZ[i]-13,-6,AZ[i]+13,ALT[i]+12.9] 
@@ -125,12 +138,25 @@ def clip_envelope(AZ, ALT, i):
         bond = [AZ[i]-18.6,ALT[i]-12.7,AZ[i]+18.6,ALT[i]+12.6] 
     elif i < 45:
         bond = [AZ[i]-39.6,ALT[i]-12.7,AZ[i]+39.6,ALT[i]+12.7] 
-    return ' '.join(str(i) for i in bond) #'xmin ymin xmax ymax' 
+    boundary = ' '.join(str(i) for i in bond) #'xmin ymin xmax ymax'
+    return boundary 
     
     
 def tc(lon,lat):
     '''
     Returns the topocentric coordinate setting in WKT format
+
+    Parameters:
+    -----------
+    lon: float
+        Longitude coordinate
+    lat: float
+        Latitude coordinate
+
+    Returns:
+    --------
+    topoCoord: str
+        WKT formatted coordinate setting
     '''
     topoCoord = (
         "PROJCS["
@@ -150,6 +176,11 @@ def tc(lon,lat):
 def set_null_values(rasterFile):
     '''
     Function to set values within raster File to NoData
+
+    Parameters:
+    -----------
+    rasterFile: string
+        Path to raster File
     '''
     outSetNull = arcpy.sa.SetNull(
         rasterFile, 
@@ -159,17 +190,30 @@ def set_null_values(rasterFile):
     outSetNull.save(rasterFile)
 
 
-def remove_readonly(func, path, excinfo):
+def remove_readonly(func, path):
     '''
     Error-catching function to handle removal of read-only folders
+
+    Parameters:
+    -----------
+    func: python function
+        Function to execute on path after chmod operation
+    path: str
+        Path to operate on
     '''
     os.chmod(path, stat.S_IWRITE)
     func(path)
-  
+
+
 def clear_scratch(scratch_dir):
     '''
     Function to clear out all files and folders from
     the scratch directory.
+
+    Parameters:
+    -----------
+    scratch_dir: str
+        Directory path to operate on
     '''
     for root, dirs, files in os.walk(scratch_dir, topdown=False):
         for name in files:
@@ -178,10 +222,21 @@ def clear_scratch(scratch_dir):
             os.chmod(os.path.join(root, name), stat.S_IWRITE)
             os.rmdir(os.path.join(root, name))
 
+
 def check_file(filename):
     ''' 
     Check that a file exists and is no longer
     being written to by another program.
+
+    Parameters:
+    -----------
+    filename: str
+        File to check
+
+    Returns:
+    --------
+    bool
+        Indicator for whether file exists or not
     '''
     
     if os.path.isfile(filename):
@@ -196,9 +251,22 @@ def check_file(filename):
         return False
     
 
+#------------------------------------------------------------------------------#
+#-------------------              Main Program              -------------------#
+#------------------------------------------------------------------------------#
+
 def mosaic(dnight, sets, filter):
     '''
     This module creates the mosaic of median filtered images for each data set.
+
+    Parameters:
+    -----------
+    dnight: str
+        Name of data night to process
+    sets: list
+        List of data sets to process
+    filter: str
+        Name of photometric filter
     '''
     #set arcpy environment variables part 2/2
     arcpy.CheckOutExtension("Spatial")
